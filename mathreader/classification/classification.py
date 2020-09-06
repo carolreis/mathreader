56537cf4fe81e546cd8be1fdeb8e8f9779e1afcd
Caroline Reis <caroline.s_reis@icloud.com>
2020-09-05 21:34:54 -0300

    Remove old models

--- mathreader/classification/classification.py
+++ mathreader/classification/classification.py
@@ -18,12 +18,6 @@ def fit(image):
     labels = helpers.get_labels()
 
     try:
-        # model = load_model('ann_models/model_all/model_16-06-2020_13-24-07.h5') # 3 > z
-        # model = load_model(config.package_path + '/ann_models/model/model_17-06-2020_20-18-12.h5') # com = menos *
-        # model = load_model(config.package_path + '/ann_models/model/model_21-06-2020_04-55-59.h5') # sem = mais *
-        # model = load_model(config.package_path + '/ann_models/model/model_21-06-2020_05-47-41.h5') # sem =  mais *
-        # model = load_model(config.package_path + '/ann_models/model/model_28-06-2020_21-45-32.h5') # sem =  mais *
-
         model = load_model(config.package_path + '/ann_models/model/model_11-07-2020_23-14-47.h5')
 
         prediction = model.predict(image)
